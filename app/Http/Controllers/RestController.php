e36bac8e590e5f14400be323aff7f827043d3cb4
Матвей Блинов <tpatata317@gmail.com>
2021-03-29 21:38:25 +0500

    add new_payday field made processing of wrong PUT request data exception README has been written

--- app/Http/Controllers/RestController.php
+++ app/Http/Controllers/RestController.php
@@ -4,31 +4,29 @@ use Illuminate\Http\Request;
 use App\Models;
 class RestController extends Controller
 {
-    public function getUsers(){
-        return response()->json(Models\User::get(),200);
-    }
-    public function getUser($userId){
-        return response()->json(Models\User::find($userId),200);
-    }
     public function getCertainRates($userId, $serviceId){
         if( (Models\User::find($userId)!==null) and (Models\Service::find($serviceId)!==null)){
             $servicesList = Models\Service::getServices($serviceId,$userId);
             if(!$servicesList->get()->isEmpty()){
-                $t2 = Models\Rate::getRates($servicesList->first()->tarif_id);
+                $ratesList = Models\Rate::getRates($servicesList->first()->tarif_id);
                 $json = array(
-                    'title' => $t2->sortBy('title')[0]['title'],
-                    'link' => $t2[0]['link'],
-                    'speed' => $t2[0]['speed'],
-                    'tarifs' => $t2
+                    'title' => $ratesList->sortBy('title')[0]['title'],
+                    'link' => $ratesList[0]['link'],
+                    'speed' => $ratesList[0]['speed'],
+                    'tarifs' => $ratesList
                 );
                 return response()->json(['result' => 'ok', 'tarifs' => $json],200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
             }
+            return response()->json(['result' => 'error'],404);
         }
         return response()->json(['result' => 'error'],404);
     }
     public function update(Request $req){
-        $t = Models\Rate::find($req->rate_id);
-        $t->update($req->all());
-        return response($t,200);
+        $rate = Models\Rate::find($req->rate_id);
+        if(is_null($rate)){
+            return response()->json(['result' => 'error'],404);
+        }
+        $rate->update($req->all());
+        return response($rate,200);
     }
 }
--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -2,14 +2,19 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 class Rate extends Model
 {
     protected $table = 'rates';
     protected $primaryKey = 'ID';
-    protected $fillable = ['ID','title','price','link','speed','pay_period','tarif_group_id'];
+    protected $fillable = ['ID','title','price','link','speed','pay_period','tarif_group_id',"new_payday"];
     public $timestamps = false;
     public $hidden = ['tarif_group_id','created_at', 'updated_at'];
     public static function getRates($rateId){
-        return Rate::where('tarif_group_id',$rateId)->get();
+        $res = Rate::where('tarif_group_id',$rateId)->get();
+        foreach($res as $i){
+            $i['new_payday'] = Carbon::today()->addMonths($i['pay_period']);
+        }
+        return $res;
     }
 }
